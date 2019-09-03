7043ad8d483d651c73ec785f24c56023bc5e642c
simon-bee <simonbee1303@gmail.com>
2019-09-03 14:03:40 +0200

    first commit

---
+++ meteo/meteo.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Mon plugin météo
+Plugin URI: https://github.com/Simon-BEE/plugin-meteo
+Description: Un plugin affichant dans la météo
+Version: 0.1
+Author: Simon
+Author URI: https://github.com/Simon-BEE/
+Text Domain: meteo
+ */
